e49cb0583117f5305037443b2ccad69529416ea2
kimmokulmala-lang <kimmo.kulmala@edu.teuva.fi>
2026-09-06 09:01:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@
 // kaikkia sivuja). Katso ohje.md, miten tämä osoite saadaan.
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxxS7oLLyOD0c3UUs_HWIDsOIo3KsGS6Jg5IUlocaRGdsW_dzeQBK-I4mRTVGJZH6frWA/exec";
 
+
 // Kuinka usein ranking.html ja monthly.html hakevat tuoreet tulokset
 // automaattisesti (millisekunteina). 60000 = 60 sekuntia.
 const REFRESH_INTERVAL_MS = 60000;
